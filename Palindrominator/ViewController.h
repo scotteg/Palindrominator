5caaf42832602f8f6ebb5de7f990eb95e813b009
Scott Gardner <scott.gardner@mac.com>
2014-07-01 19:19:57 -0500

    Initial commit

---
+++ Palindrominator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Palindrominator
+//
+//  Created by Scott Gardner on 7/1/14.
+//  Copyright (c) 2014 Optimac, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PalindrominatorTests/PalindrominatorTests.m
@@ -0,0 +1,34 @@
+//
+//  PalindrominatorTests.m
+//  PalindrominatorTests
+//
+//  Created by Scott Gardner on 7/1/14.
+//  Copyright (c) 2014 Optimac, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PalindrominatorTests : XCTestCase
+
+@end
+
+@implementation PalindrominatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
